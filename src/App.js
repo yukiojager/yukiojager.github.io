49b5d87464c6dd2aef9249df0c2d07eb4a0d429e
Sebastian Vera <100543895+verajs@users.noreply.github.com>
2022-05-23 16:10:57 -0400

    v1.0.0

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ const App = () => {
                   </div>
                 </a>
               </li>
-              {/* momento de github */}
+
               <li className="licito">
                 <a
                   className="acito"
